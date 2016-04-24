5b7eb5ea6d2a85a3c51955af3a1cfb9c0b85fb1e
spskauynnda <1033515355@qq.com>
2016-04-24 09:44:40 +0800

    第二次提交  完成基本框架及第一个样例

--- work2.js
+++ work2.js
@@ -1,3 +1,29 @@
-/**
- * Created by Administrator on 2016/4/23.
- */
+function list(dtNode) {
+    var dlNode = dtNode.parentNode;
+    if (dlNode.className == "open") {
+        dlNode.className = "";
+    } else {
+        dlNode.className = "open";
+    }
+}
+
+function t1btn_1() {
+    var oT1bg = document.getElementById("t1bg");
+    oT1bg.style.width = "200px";
+}
+function t1btn_2() {
+    var oT1bg = document.getElementById("t1bg");
+    oT1bg.style.height = "200px";
+}
+function t1btn_3() {
+    var oT1bg = document.getElementById("t1bg");
+    oT1bg.style.background = "#09F";
+}
+function t1btn_4() {
+    var oT1bg = document.getElementById("t1bg");
+    oT1bg.style.display = "none";
+}
+function t1btn_5() {
+    var oT1bg = document.getElementById("t1bg");
+    oT1bg.style.display = "block";
+}
